dbcca330d2aa379635d1d26e61519867677d5d23
Fraser Solomon <fmsolomon1@gmail.com>
2018-09-19 16:10:51 +1000

    store persists through local storage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { persistor, store } from './config/store';
+import LoadingView from './components/LoadingView'
 
-import './index.css';
-import App from './App';
-import store from './config/store'
+import './index.css'
+import App from './App'
 
-const app = <Provider store={store}> 
-    <BrowserRouter> 
-        <App />
-    </BrowserRouter>
+const app = 
+<Provider store={store}> 
+    <PersistGate loading={<LoadingView />} persistor={persistor}>
+        <BrowserRouter> 
+            <App />
+        </BrowserRouter>
+    </PersistGate>
 </Provider>
 
 ReactDOM.render(app, document.getElementById('root'));
