358f9d620f6119733ec897705ba2e69ef583a9b6
Fraser Solomon <fmsolomon1@gmail.com>
2018-09-19 16:48:56 +1000

    cart slightly styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import AddBtn from '../addBtn'
 import './styles.css'
 
-const ProductListItem = (props) => {
+const Card = (props) => {
     return ( 
         <div className="card__container"> 
             <img className="card__img"
@@ -14,15 +14,19 @@ const ProductListItem = (props) => {
             <p className="card__name">{props.product.name}</p> 
             <div className="card__desc">{props.product.description}</div>
             <div className="card__price-button">
-                <AddBtn 
-                    cartItem={props.cartItem} 
-                    product={props.product}
-                    addToCart={props.addToCart} 
-                />
+                { props.addToCart ?
+                    <AddBtn 
+                        cartItem={props.cartItem} 
+                        product={props.product}
+                        addToCart={props.addToCart} 
+                    />
+                    :
+                    <p> quantity: {props.product.quantity} </p>
+                }
                 <p className="card__price">${props.product.price}</p>
             </div>
         </div>
      );
 }
  
-export default ProductListItem;
+export default Card;
